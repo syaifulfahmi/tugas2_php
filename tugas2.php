c55fad73cdc996e9262e454fb671087bbae3aa82
syaifulfahmi <52196122+syaifulfahmi@users.noreply.github.com>
2020-10-07 12:08:17 +0700

    Add files via upload

---
+++ tugas2.php
@@ -0,0 +1,15 @@
+<?php
+
+$hari1 = 36;
+$jml_orang1 = 12;
+$jml_orang2 = 24;
+$hari2 = $jml_orang1/$jml_orang2*$hari1;
+
+echo "36 Hari = 12 Pekerja <br>";
+echo "X Hari = 24 Pekerja <br>";
+echo "X = ...? <br>";
+echo "Jawaban : ".$hari2;
+
+
+
+?>
